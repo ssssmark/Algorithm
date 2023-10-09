57a8f84e8152d200f1043ae388bcf6aed8dab711
ssssmark <111336914+ssssmark@users.noreply.github.com>
2023-10-09 20:00:47 +0800

    math

---
+++ 数学/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<conio.h>
+using namespace std;
+#define KEY_LEFT 65
+int main()
+{
+	char a=_getch();
+	char b = _getch();
+	cout << (int)a << (int)b;
+	return 0;
+}
